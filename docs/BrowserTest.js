02132d4cea782d9fa93a27aa392a0b2f30e583a8
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2018-07-30 15:25:23 -0700

    NEW: run the test in your browser on github

---
+++ docs/BrowserTest.js
@@ -0,0 +1,148 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/*!****************************!*\
+  !*** ./BrowserTest.coffee ***!
+  \****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+var out;
+
+out = __webpack_require__(/*! ./TestCompatibility */ 1);
+
+document.body.innerHTML = "<pre>\nCaffeineScript compatible: " + (!!out.success) + "\nDetails:\n  " + (JSON.stringify(out, null, 4)) + "\n</pre>";
+
+
+/***/ }),
+/* 1 */
+/*!**********************************!*\
+  !*** ./TestCompatibility.coffee ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var out, tryThis;
+
+tryThis = function(out, name, toEval) {
+  var error;
+  try {
+    eval(toEval);
+    return out[name + "Ok"] = true;
+  } catch (error1) {
+    error = error1;
+    out.success = false;
+    return out[name + "Error"] = error.message;
+  }
+};
+
+out = {
+  success: true
+};
+
+tryThis(out, "class", "(class Foo extends Object {constructor() {super(...arguments);this.bar = 123;}});");
+
+tryThis(out, "interpolation", "(`1 + 2 = ${1 + 2}`)");
+
+tryThis(out, "boundFunction", "(() => {})");
+
+tryThis(out, "spred", "(function (...a) {return foo(...a);});");
+
+tryThis(out, "let", "(function () {let a = 1;})");
+
+tryThis(out, "bareFunction", "(a => a + 1)");
+
+tryThis(out, "structuring", "(function () {b = {a, b, c};})");
+
+tryThis(out, "destructuring", "(function () {let {length} = [];})");
+
+module.exports = out;
+
+
+/***/ })
+/******/ ]);
